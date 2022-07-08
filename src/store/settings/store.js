d734d95cc3ff45088438eb7d886f3b6654a3e9fc
Aleksandr Manekin <alexusmai@gmail.com>
2022-07-08 21:20:04 +0300

    up app version

--- src/store/settings/store.js
+++ src/store/settings/store.js
@@ -29,7 +29,7 @@ export default {
             acl: null,
 
             // App version
-            version: '3.0.1',
+            version: '3.0.2',
 
             // axios headers
             headers: {},
